42296de79084edf7e9c61b004b898522434f802d
Jamie Mansfield <dev@jamierocks.uk>
2016-12-15 18:32:17 +0000

    travis: Clone repo to retain history

--- scripts/buildTravis.sh
+++ scripts/buildTravis.sh
@@ -3,20 +3,20 @@
 LEX_DEPLOY=https://github.com/LexBot/Deploy.git
 DEPLOY_SCRIPTS=/tmp/mbs-cs/deploy
 
-# Let's build the docs
-mkdocs build
-
 # Initialise the ssh-agent
 eval $(ssh-agent)
 
 # Set up our Git environment
 $DEPLOY_SCRIPTS/setup_git
 
+# Let's clone the docs repo so that we can commit ontop it
+git clone -b gh-pages git@github.com:mbs-cs/learn.git site
+
+# Let's build the docs
+mkdocs build
+
 # Deploy
 cd site
-git init
-git remote add origin git@github.com:mbs-cs/learn.git
-git checkout --orphan gh-pages
 git add .
 git commit -q -m "Deploy $(date)"
 git push -q origin gh-pages
